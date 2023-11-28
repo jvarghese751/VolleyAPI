063ee58a21903063fdf954d0ea919f0cfc8db443
Joel Varghese <Varghese-J2@ulster.ac.uk>
2023-11-28 11:15:22 +0000

    Initial commit

---
+++ app/src/main/java/com/example/volleyapi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.volleyapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String API_KEY = e11cf1f4deea966736d6f00d46764dc0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
