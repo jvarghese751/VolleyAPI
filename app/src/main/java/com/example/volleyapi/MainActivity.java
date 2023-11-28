e9d84d3cdf8c6c3274fdc880fdfbdf40b1b6cfa9
jvarghese751 <varghese-J2@ulster.ac.uk>
2023-11-28 12:07:49 +0000

    Volley

--- app/src/main/java/com/example/volleyapi/MainActivity.java
+++ app/src/main/java/com/example/volleyapi/MainActivity.java
@@ -3,13 +3,46 @@ package com.example.volleyapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String API_KEY = e11cf1f4deea966736d6f00d46764dc0;
+    private static final String API_KEY = "e11cf1f4deea966736d6f00d46764dc0";
+    TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtView = (TextView) findViewById(R.id.weather_txt);
+
+        //Instantiate the Request Queue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=e11cf1f4deea966736d6f00d46764dc0";
+
+        //Request a string response from url
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //Display the first 500 characters of the response string.
+                        txtView.setText("Response is: " + response.substring(0));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                txtView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
